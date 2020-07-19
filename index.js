a36cfd037ea9e2347f778d8360a61c7204602ceb
Nila <nilakrishnan27@gmail.com>
2020-07-19 17:31:05 -0500

    Add sql databse configuration

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express');
+const express = require('express')
+const pgp = require('pg-promise')()
+
 const app = express();
 
 const PORT = process.env.PORT || 80;
 
+var db = pgp('postgres://root:project123!@localhost:5542/database')
+
 app.get('/', (req, res) => {
-    res.send('hi');
+  db.one('SELECT *', 1)
+  .then((data) => {
+    res.send(data.value)
+  })
+  .catch(function (error) {
+    res.send(error)
+  })
 });
 
 app.listen(PORT, () => {
