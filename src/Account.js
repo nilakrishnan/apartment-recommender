5a0c73a1c0fe88b4e2e123b277cc7e97cd0d012a
anchita2 <anchita2@illinois.edu>
2020-07-26 15:26:27 -0700

    it worksgit add src/Account.js

--- src/Account.js
+++ src/Account.js
@@ -20,19 +20,25 @@ class Account extends React.Component {
       if (!user[0]) {
         fetch('/addUser', {
           method: 'POST',
-          body: {
+          body: JSON.stringify({
             "userId": data.email,
             "firstName": data.firstName,
             "lastName": data.lastName
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
           }
-        }).then(res => res.json()).then(newUser => this.setState({
-            email: data.email,
-            firstName: data.firstName,
-            lastName: data.lastName
-          })
-        )
+        // }).then(res => res.json()).then(json => console.log(json));
+      }).then(res => res.text())
+        this.setState({
+              email: data.email,
+              firstName: data.firstName,
+              lastName: data.lastName
+        })
       } else {
         this.setState({
+          // this is for if there is already an account created
+          // we are saving that information
           email: user[0].UserId,
           firstName: user[0].FirstName,
           lastName: user[0].LastName
@@ -45,6 +51,7 @@ class Account extends React.Component {
     return (
       <div className="Account">
         <div className="Profile">
+          <p>{this.state.email}</p>
           <p>{this.state.firstName}</p>
           <p>{this.state.lastName}</p>
         </div>
